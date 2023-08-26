059cd643a27120581ce31f90b81df405df8df602
JulioEVA <julio-emmanuel2011@hotmail.com>
2023-08-25 21:39:45 -0700

    Hotfix: Only call the API once at mounting

--- src/components/Main.js
+++ src/components/Main.js
@@ -14,7 +14,7 @@ function Main(props) {
       setUserDescription(user.about);
       setUserAvatar(user.avatar);
     });
-  }, [userName, userDescription, userAvatar]);
+  }, []);
 
   React.useEffect(() => {
     api.getInitialCards().then((cards) => {
